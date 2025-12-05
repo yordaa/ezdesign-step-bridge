e0d0e1cabff335342c055544f0788fc7fb74d445
Yang Song <yangsong1442@gmail.com>
2025-12-05 11:50:20 +0800

    feat: Update README and source files for EzDesign to STEP Converter
    This commit revises the README.md to reflect the new focus on the EzDesign to STEP conversion tool, detailing its functionality, licensing, and usage instructions. Additionally, it updates the copyright information and proprietary notices in the source files within the `tools/json2step/` directory, ensuring clarity on ownership and usage rights. These changes aim to enhance documentation and compliance for the project.

--- tools/json2step/json2step.cxx
+++ tools/json2step/json2step.cxx
@@ -1,17 +1,10 @@
 // Created on: 2025
-// Created by: OCCT json2step tool
-// Copyright (c) 2025 OPEN CASCADE SAS
+// Created by: EzDesign to STEP Converter
+// Copyright (c) 2025 Yang Song. All rights reserved.
 //
-// This file is part of Open CASCADE Technology software library.
-//
-// This library is free software; you can redistribute it and/or modify it under
-// the terms of the GNU Lesser General Public License version 2.1 as published
-// by the Free Software Foundation, with special exception defined in the file
-// OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
-// distribution for complete text of the license and disclaimer of any warranty.
-//
-// Alternatively, this file may be used under the terms of Open CASCADE
-// commercial license or contractual agreement.
+// This file is proprietary and confidential. Unauthorized copying, modification,
+// distribution, or use of this file, via any medium, is strictly prohibited.
+// See LICENSE file in this directory for terms and conditions.
 
 #include "EzDesignJsonReader.hxx"
 #include "EzDesignToOCCTConverter.hxx"
