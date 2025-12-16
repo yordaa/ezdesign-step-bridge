0b32873d7f2afaa36c01f1b497deb0cb35d19eb5
Yang Song <yangsong1442@gmail.com>
2025-12-17 00:50:20 +0800

    Fix: Use VCPKG_TARGET_IS_* variables for platform detection

--- ports/ezd2step/portfile.cmake
+++ ports/ezd2step/portfile.cmake
@@ -4,14 +4,14 @@
 # Version is defined in vcpkg.json and available as ${VERSION}
 # For v1.0.0, VERSION will be "1.0.0"
 
-# Detect platform and set platform-specific variables
-if(APPLE AND CMAKE_SYSTEM_PROCESSOR MATCHES "arm64")
+# Detect platform using vcpkg variables (not APPLE/WIN32 which don't work in script mode)
+if(VCPKG_TARGET_IS_OSX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
     set(PLATFORM "macos-arm64")
     set(ARCHIVE_SUFFIX ".tar.gz")
     set(ARCHIVE_NAME "ezd2step-${VERSION}-macos-arm64.tar.gz")
     # SHA512 hash for macOS arm64 bundle (v1.0.0)
     set(ARCHIVE_SHA512 "f8b856f56cac6b09169d7170f47045b17e5663a7b137d5b8bc484297e4e84bbc038a1945e6a2a15eecb315b90f4b2a25e6420d1d9f91433b77b96cdb19c86062")
-elseif(WIN32 AND CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
+elseif(VCPKG_TARGET_IS_WINDOWS AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
     set(PLATFORM "windows-x64")
     set(ARCHIVE_SUFFIX ".zip")
     set(ARCHIVE_NAME "ezd2step-${VERSION}-windows-x64.zip")
@@ -19,7 +19,7 @@ elseif(WIN32 AND CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
     set(ARCHIVE_SHA512 "")
     message(FATAL_ERROR "Windows x64 bundle not yet available. Only macOS arm64 is supported in v1.0.0")
 else()
-    message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME} ${CMAKE_SYSTEM_PROCESSOR}. Supported: macOS arm64")
+    message(FATAL_ERROR "Unsupported platform: ${VCPKG_CMAKE_SYSTEM_NAME} ${VCPKG_TARGET_ARCHITECTURE}. Supported: macOS arm64")
 endif()
 
 # Download bundle from GitHub Releases
@@ -53,4 +53,3 @@ endif()
 
 # Cleanup temp extraction directory
 file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/src")
-
