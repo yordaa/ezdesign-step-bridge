cff86a045f9231562ba76472cdb3182f3cf92281
Yang Song <yangsong1442@gmail.com>
2026-07-25 13:28:39 +0800

    fix(ezd2step): remove remaining vendored OCCT

--- src/TKV3d/TKV3d_pch.hxx
+++
@@ -1,9 +0,0 @@
-#ifndef TKTKV3d_PCH_H
-#define TKTKV3d_PCH_H
-
-#include <AIS_InteractiveObject.hxx>
-#include <PrsMgr_PresentableObject.hxx>
-#include <SelectMgr_EntityOwner.hxx>
-#include <SelectMgr_SelectableObject.hxx>
-
-#endif // TKTKV3d_PCH_H
